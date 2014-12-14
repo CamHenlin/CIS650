41fd6a5ceafc0ed533faa7b782d2af7e0be41144
Cameron Henlin <cam.henlin@gmail.com>
2014-12-14 12:41:58 -0800

    updated proj0 to uses blessed

--- Project0/app.js
+++ Project0/app.js
@@ -2,6 +2,37 @@ var os = require('os');
 var http = require('http');
 var express = require('express');
 var app = express();
+var blessed = require('blessed');
+
+// Create a screen object.
+var screen = blessed.screen();
+
+// Create a box perfectly centered horizontally and vertically.
+var box = blessed.box({
+	top: 'center',
+	left: 'center',
+	width: '50%',
+	height: '50%',
+	content: '',
+	tags: true,
+	border: {
+		type: 'line'
+	},
+	style: {
+		fg: 'white',
+		bg: 'black',
+		border: {
+			fg: '#f0f0f0'
+		},
+		hover: {
+			bg: 'black'
+		}
+	}
+});
+
+// Append our box to the screen.
+screen.append(box);
+
 app.set('port', process.env.PORT || 3000);
 
 var ifaces = os.networkInterfaces();
@@ -10,13 +41,13 @@ var nodes = [];
 
 // find our own ip address:
 for (var dev in ifaces) {
-	if (dev !== 'en1') { // may need to modify to be eth0 or wlan0 on the rpi, en1 is for wifi on a macintosh
+	if (dev !== 'vmnet8') { // may need to modify to be eth0 or wlan0 on the rpi, en1 is for wifi on a macintosh
 		continue;
 	}
 	var alias = 0;
 	ifaces[dev].forEach(function(details) {
 		if (details.family=='IPv4') {
-			console.log(dev + (alias? ':' + alias: ''), details.address);
+			box.setContent(dev + (alias? ':' + alias: ''), details.address);
 			++alias;
 
 			my_ip = details.address;
@@ -24,8 +55,9 @@ for (var dev in ifaces) {
 	});
 }
 
-var lan = my_ip.match(/[0-255]+\.[0-255]+\.[0-255]+./);
-console.log('this node (' + my_ip + ') will attempt to send its token to other nodes on network: ' + lan + "xxx" + ":" + app.get('port'));
+var lan = my_ip.match(/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}/)[0];
+box.setContent('this node (' + my_ip + ') will attempt to send its token to other nodes on network: ' + lan + "xxx" + ":" + app.get('port'));
+screen.render();
 
 /**
  * [description]
@@ -34,6 +66,8 @@ console.log('this node (' + my_ip + ') will attempt to send its token to other n
  * @return {[type]}      [description]
  */
 app.get('/is_node', function(req, res) {
+	box.setContent('another node is searching for nodes to send to!');
+	screen.render();
 	res.write(JSON.stringify({ response: true })); // could be anything, even just an OK response
 	res.end();
 });
@@ -48,6 +82,10 @@ app.get('/transmit_token/:token', function(req, res) {
 	var token = req.param('token');
 	res.write("token received"); // could be anything, even just an OK response
 	res.end();
+
+	box.setContent('this node now has the token.');
+	box.style.bg = 'green';
+	screen.render();
 
 	for (var i = 1; i < 255; i++) {
 		if (lan + i === my_ip) { // skip our own ip address
@@ -95,11 +133,15 @@ app.get('/transmit_token/:token', function(req, res) {
 	// wait 5 seconds then pass our token to some other random node:
 	setTimeout(function() {
 		if (!nodes.length) {
-			console.log('no nodes to transmit token to!');
+			box.setContent('no nodes to transmit token to!');
+			screen.render();
+
 			return;
 		}
 
-		var target = Math.floor(Math.random() * nodes.length);
+		var target = nodes[Math.floor(Math.random() * nodes.length)];
+		box.setContent('this node is sending to ' + target);
+		screen.render();
 
 		var options = {
 			host: lan + target,
@@ -113,7 +155,9 @@ app.get('/transmit_token/:token', function(req, res) {
 				bodydata.push(data);
 			}).on('end', function() {
 				var body = Buffer.concat(bodydata);
-
+				box.setContent('this node just got rid of the token');
+				box.style.bg = 'black';
+				screen.render();
 				// we now have a body object, should be "token received"
 			});
 		});
@@ -130,13 +174,25 @@ app.get('/transmit_token/:token', function(req, res) {
 	}, 5000);
 });
 
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+	return process.exit(0);
+});
+
+// Focus our element.
+box.focus();
+
+// Render the screen.
+screen.render();
+
 /**
  * [description]
- * @param  {[type]} ) {	console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));} [description]
+ * @param  {[type]} ) {	box.setContent('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));} [description]
  * @return {[type]}   [description]
  */
 app.listen(app.get('port'), function() {
-	console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+	box.setContent('Express server listening on port ' + app.get('port') + ' in ' + app.get('env') + ' mode');
+	screen.render();
 });
 
 module.exports = app;
